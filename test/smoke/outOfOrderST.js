6af0c339358e8bd723d2c94e4475c3d80b650d5f
aleidasarzuri <ale.sarzuri@hotmail.com>
2015-08-24 21:01:23 -0400

    smoke test of out of order

--- test/smoke/outOfOrderST.js
+++ test/smoke/outOfOrderST.js
@@ -8,135 +8,147 @@ var config = require('..\\..\\config\\outOfOrderConfig.json');
 
 describe('Smoke Test 1.0 - Out of order', function(){
     /*Variables*/
-    var token;
-    var roomId;
-    var oooId;
+    this.timeout(5000);
+    this.slow(4000);
 
-    var oooCont = {
-        "from": "2015-08-23T20:30:00.000Z",
-        "to": "2015-08-23T21:00:00.000Z",
-        "title": "Temporarily Out of Order",
-        "sendEmail": false
+    var oooCont =
+    {
+        from: "2015-08-24T21:30:00.000Z",
+        to: "2015-08-24T22:00:00.000Z",
+        title: "Temporarily Out of Order",
+        sendEmail: false
     };
     var oooEdit = {
-        "from": "2015-08-23T22:30:00.000Z",
-        "to": "2015-08-23T23:00:00.000Z",
+        "from": "2015-08-24T22:30:00.000Z",
+        "to": "2015-08-24T23:00:00.000Z",
         "title": "Temporarily Out of Order",
         "sendEmail": false
 
     };
 
-    /*Get token - Pre condition for some smoke test cases*/
-    before('before',function(){
-        outOfOrders
-            .getToken(config)
-            .end(function(error, resp) {
-                token = resp.body.token;
-            });
-    });
-    /**Smoke Test Case
-     *Title: GET all out of order
-     */
     it('Get all out of order', function(done){
         outOfOrders
             .getOutOfOrders()
             .end(function(error1, resp1){
                 expect(resp1.status).to.equal(200);
-
                 done();
             });
     });
     /**Smoke Test Case
-   *Title: GET out of order from a specific room
-   */
-    it('Get a out of order', function(done){
-        outOfOrders
-            .getOutOfOrderbyRoom('0000')
-            .end(function(error2, resp2){
-                var response = resp2.status;
-                expect(response).to.equal(404);
-                done();
-            });
+     *Title: GET out of order from a specific room
+     */
+    it('Get a out of order from a service from a room', function(done){
+        //room id
+         outOfOrders
+            .getRoomId()
+            .end(function(e,r){
+            var roomId = r.body[0]._id;
+            var servId = r.body[0].serviceId;
+                 console.log(roomId);
+            outOfOrders
+             .getOutOfOrderbyRoom(servId,roomId)
+             .end(function(error2, resp2){
+                    expect(resp2.status).to.equal(200);
+                    done();
+              });
+         });
     });
     /**Test Case
      *Title: CREATE out of order from a specific room
      */
     it('Create a out order', function(done){
         /*Test Case*/
+        /*Get token - Pre condition for some smoke test cases*/
         outOfOrders
-           .createOutOfOrder(oooCont, token)
-            .end(function(error3, resp3){
-                roomId = resp3.body.roomId;
-                var response = resp3.status;
-                 expect(response).to.equal(404);
-                 /*Post condition*/
-                 outOfOrders
-                    .deleteOutOfOrder(roomId, token)
-                    .end(function(error4, resp4){
-                     var respo = resp4.status;
-                     expect(respo).to.equal(404);
-                         done();
-                 });
+            .getToken(config)//token
+            .end(function(error, resp) {
+               var token = resp.body.token;
+                /*Create a out of order from a service [serviceId] from a room [roomId]*/
+               outOfOrders
+               .createOutOfOrder(0000,0000,oooCont,token)
+               .end(function(error3, resp3){
+                    var response = resp3.status;
+                    expect(response).to.equal(404);
 
-         });
+                     done();
 
+            });
+        });
     });
     /**Test Case
-     *Title: DELETE out of order
+     *Title: Get out of order by id from a service from a room
      */
-    it('Delete a out of order', function(done){
+    it('Get a out order by id', function(done) {
         /*Test Case*/
+        /* Get roomId and Service Id*/
+        var roomId;
+        var oooId;
+        var servId;
         outOfOrders
-            .createOutOfOrder(oooCont, token)
-            .end(function(error5, resp5){
-                roomId = resp5.body.roomId;
-                var response = resp5.status;
-                expect(response).to.equal(404);
-                /*Post condition*/
+            .getRoomId()
+            .end(function(err6,resp6){
+                servId=resp6.body[0].serviceId;
                 outOfOrders
-                    .deleteOutOfOrder(roomId, token)
-                    .end(function(error6, resp6){
-                        var respo = resp6.status;
-                        expect(respo).to.equal(404);
-                        done();
+                    .getOutOfOrders()
+                    .end(function (err5, res5) {
+                        roomId = res5.body[0].roomId;
+                        oooId = res5.body[0]._id;
+                        outOfOrders
+                            .getOutOfOrderById(servId,roomId,oooId)
+                            .end(function(error3, res3){
+                                var response = res3.status;
+                                expect(response).to.equal(200);
+                                done();
+                            });
+
                     });
+
             });
 
     });
+    /**Test Case
+     *Title: Update out of order by id from a service from a room
+     */
+    it('Update a out order', function(done){
+        /*Test Case*/
+        /*Get token - Pre condition for some smoke test cases*/
+        outOfOrders
+            .getToken(config)//token
+            .end(function(error, resp) {
+                var token = resp.body.token;
 
-    describe('Smoke Test 1.1- Out of order', function(){
-        /*Pre condition*/
-        beforeEach('Before each-create',function(done){
-            outOfOrders
-                 .createOutOfOrder(oooCont, token)
-                 .end(function(error7, resp7){
-                    roomId = resp7.body.roomId;
-                    oooId = resp7.body._id;
-                    done();
+                outOfOrders//update
+                .updateOutOfOrder(0000,0000,oooEdit,token,0000)
+                .end(function(error3, resp3){
+                     var response = resp3.status;
+                     expect(response).to.equal(404);
+
+                      done();
+
                 });
 
         });
-        /*Post condition*/
-        afterEach('After Each',function(){
-            outOfOrders
-                .deleteOutOfOrder(roomId,oooId, token)
-                .end(function(error10, resp10){
-                });
-        });
-        /**Test Case
-         *Title: UPDATE a room with out of order
-         */
-        it('Update a room with out of order', function(done){
-            /*Pre condition*/
-            outOfOrders
-                .updateOutOfOrder(roomId,oooId,oooEdit, token)
-                .end(function(error8, resp8){
-                    var response = resp8.status;
-                    expect(response).to.equal(404);
-                          done();
-                     });
-        });
+    });
+    /**Test Case
+     *Title: Delete out of order
+     */
+    it('Delete a out order', function(done){
+        /*Test Case*/
+        /*Get token - Pre condition for some smoke test cases*/
+        outOfOrders
+            .getToken(config)//token
+            .end(function(error, resp) {
+                var token = resp.body.token;
 
+                outOfOrders//delete
+                .deleteOutOfOrder(0000, token)
+                .end(function(error4, resp4){
+                     var respo = resp4.status;
+                     expect(respo).to.equal(404);
+                     done();
+
+                });
+            });
     });
 
 });
