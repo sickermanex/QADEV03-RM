a4806df1cce01936ba5ac93b1cc64d9f5cbee0cb
lufercc <lufer_cc@hotmail.com>
2015-08-26 15:51:41 -0400

    fixed the smoke before and delete one describe

--- test/smoke/servicesSt.js
+++ test/smoke/servicesSt.js
@@ -9,11 +9,12 @@ var settings = require('..\\..\\settings.json');
 /**
  * var needed
  * */
-var login = {
-    "username": settings.domain + "\\" + settings.roomManagerAccount,
-    "password": settings.roomManagerPassword,
-    "authentication": "ldap"
-};
+/**
+ *this var is to add the exchange services
+ * used in the post test and the smoke test before
+ * in case that is not there added any services
+ */
+
 var loginExchange = {
         "username": settings.exchangeAccount,
         "password": settings.exchangeAccountPassword,
@@ -22,118 +23,57 @@ var loginExchange = {
 
 var serviceId;
 var token;
-var serviceType;
+var serviceType = settings.typeservice;
 
 
 
 describe('Room Manager Smoke Test:', function() {
-    this.timeout(50000);
-    this.slow(40000);
-
+    this.timeout(settings.setDelayTime);
+    this.slow(settings.setErrorMaxTime);
     /**
-    get token and services ID
-    */
-    before(function (done) {
+     * this before get the token
+     * for a specified user on the setting.json
+     * */
+    before('Setting the token', function(done){
         tokenLib
-            .getToken(login)
-            .end(function (err, res) {
-                token = 'jwt ' + res.body.token;
-                servicesLib
-                    .getServices(token)
-                    .end(function(err, res){
-                        serviceId =  res.body[0]._id;
-                        serviceType = res.body[0].type;
+            .getToken(done, function(){
+                token = arguments[0];
+            });
+    });
+    describe( 'smoke test room manager ' , function () {
+        /**
+         * get the service Id
+         * if there is not addes any services
+         * this before add a services
+         * */
+         before(function (done) {
+            servicesLib
+                .getServices(token)
+                .end(function(err, res){
+                    serviceId =  res.body[0]._id;
+                    if(!serviceId)
+                    {
+                        servicesLib
+                            .postservices(token,serviceType,loginExchange)
+                            .end(function(err, res){
+                                serviceId = res.body._id;
+                                done();
+                            });
+                    }else
                         done();
-                    });
-            });
-    });
-
-    /**
-    verify that the services api is present
-    */
-
-    it('verify that the API services exist', function(done){
-        servicesLib
-            .getServices(token)
-            .end(function(err, res){
-                var status = res.status;
-                expect(status).to.equal(200);
-                done();
-            });
-    });
-
-    /**
-     * verify that the API services by Id exist
-     * */
-
-    it('verify that the API services by Id exist', function(done){
-       servicesLib
-           .getServicesById(token , serviceId)
-           .end(function(err , res){
-               var status = res.status;
-               expect(status).to.equal(200);
-               done();
-           })
-    });
-
-    /**
-     * verify that the API get services-type exist
-     * */
-
-    it('verify that the API service-Type exist', function(done){
-       servicesLib
-           .getserviceType()
-           .end(function(err, res){
-               var status = res.status;
-               expect(status).to.equal(200);
-               done();
-           })
-    });
-
-    /**
-     * verify that the API get services by type exist
-     * */
-
-    it('verify that the API get servicesbytype exist', function(done){
-        servicesLib
-            .getservicebyType(serviceType,token)
-            .end(function(err, res){
-                var status = res.status;
-                expect(status).to.equal(200);
-                done();
-            })
-    });
-    /**
-     *this describe test the existence of the API when did post and delete
-     */
-    describe('create and delete services' ,  function(){
-        var serviceIDdel;
-        var newservice;
-        before(function(done){
-            servicesLib
-                .postservices(token,serviceType,loginExchange)
-                .end(function(err, ress){
-                    serviceIDdel = ress.body._id;
-                    done();
                 });
-        });
-        after(function(done){
-            servicesLib
-                .deleteservice(token,newservice)
-                .end(function(err, res){
-                    done();
-                });
-        });
+            });
+
 
         /**
-         *this describe test the existence of the API when did post
+         * verify that the services api is present
+         * end point : '/services'
          */
 
-        it('verify that the API to be post exist', function(done){
+        it('verify that the API services exist', function(done){
             servicesLib
-                .postservices(token,serviceType,loginExchange)
+                .getServices(token)
                 .end(function(err, res){
-                    newservice = res.body._id;
                     var status = res.status;
                     expect(status).to.equal(200);
                     done();
@@ -141,18 +81,80 @@ describe('Room Manager Smoke Test:', function() {
         });
 
         /**
-         *this describe test the existence of the API when did delete
-         */
-        it('verify that the API to be delete exist', function(done){
+         * verify that the API services by Id exist
+         * end point : '/services/{:serviceId}'
+         * */
+
+        it('verify that the API services by Id exist', function(done){
             servicesLib
-                .deleteservice(token,serviceIDdel)
+                .getServicesById(token , serviceId)
+                .end(function(err , res){
+                    var status = res.status;
+                    expect(status).to.equal(200);
+                    done();
+                })
+        });
+
+        /**
+         * verify that the API get services-type exist
+         * end point : '/service-types'
+         * */
+
+        it('verify that the API service-Type exist', function(done){
+            servicesLib
+                .getserviceType()
                 .end(function(err, res){
                     var status = res.status;
                     expect(status).to.equal(200);
                     done();
-                });
+                })
         });
 
-    });
+        /**
+         * verify that the API get services filtering by Type
+         * end point : '/services?type={service type} '
+         * */
 
+        it('verify that the API get servicesbytype exist', function(done){
+            servicesLib
+                .getservicebyType(serviceType,token)
+                .end(function(err, res){
+                    var status = res.status;
+                    expect(status).to.equal(200);
+                    done();
+                })
+        });
+
+
+        /**
+         *this test cases delete the services added
+         * end point : '/services/{:serviceId}'
+         */
+
+         it('verify that the API to be delete exist', function(done){
+             servicesLib
+                 .deleteservice(token,serviceId)
+                 .end(function(err, res){
+                     var status = res.status;
+                     expect(status).to.equal(200);
+                     done();
+                 });
+         });
+
+        /**
+         *this test cases add a new services
+         * end point : '/services?type={service type} '
+         */
+         it('verify that the API to be post exist', function(done){
+             servicesLib
+                 .postservices(token,serviceType,loginExchange)
+                 .end(function(err, res){
+                     serviceId = res.body._id;
+                     var status = res.status;
+                     expect(status).to.equal(200);
+                     done();
+                 });
+         });
+
+    });
 });
