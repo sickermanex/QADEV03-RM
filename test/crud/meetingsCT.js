f0f39f5a8378293d6acb2bd13666b79ef1870882
sickermanex <sickermanex@gmail.com>
2015-08-26 17:02:27 -0400

    Applying the changes suggested by the trainer

--- test/crud/meetingsCT.js
+++ test/crud/meetingsCT.js
@@ -2,7 +2,9 @@
 var expect = require('chai').expect;
 var meetings = require('..\\..\\lib\\meetingsLib');
 var rooms = require('..\\..\\lib\\roomsLib');
-var services = require('..\\..\\lib\\serviceLib');
+var services = require('..\\..\\lib\\servicesLib');
+var tokenLib = require('..\\..\\lib\\tokenLib');
+var settings = require('..\\..\\settings.json');
 
 var roomId = [];
 var meetingId;
@@ -10,48 +12,65 @@ var serviceId;
 var authToken;
 var response;
 
-describe('..:: Meetings ::..',function(){	
+describe('..:: Meetings CRUD Test cases ::..',function(){	
 	
-	this.timeout(10000);
-
-	before(function(done){
-		rooms
-			.getAllRooms()
-			.end(function(er,res){
-				response = res.body;
-				for(var i in response)
-				{
-					roomId.push(response[i]._id);						
-				}
-				services
-					.getToken()
-					.end(function(err,res){
-						response = res.body;
-						authToken = "jwt "+response.token;	
-						services
-							.getServices(authToken)
-							.end(function(err,res){
-								response = res.body;
-								serviceId = response[0]._id;								
-								done();
-							});
-					});
-			});		
+	this.timeout(settings.setDelayTime);
+	this.slow(settings.setErrorMaxTime);
+	
+	/**
+	Get the authorization token
+	*/
+	
+	before('Obtain the authorization token access to another endpoints',function(done){
+		tokenLib
+			.getToken(done,function(){
+				authToken = arguments[0];				
+			});			
 	});
 	
+	/**
+	Get the IDs from the exchange service and the rooms
+	*/
 	
-	it('3.1 READ all the meetings from a room',function(done){
-		meetings
+	before('Getting the Service and the Rooms IDs'function(done){
+		services
+			.getServicesId(authToken)
+			.end(function(err,res){
+				response = res.body;
+				serviceId = response[0]._id;
+				rooms
+					.getAllRooms()
+					.end(function(err,res){
+						response = res.body;
+						for(var i in response)
+						{
+							roomId.push(response[i]._id);							
+						}
+						done();
+					});
+			});					
+	});
+	
+	/**
+	Test case 1: Get all the meetings from a room
+	*/	
+	it('READ all the meetings from a room',function(done){
+		meetings			
 			.getAllRoomMeetings(serviceId,roomId[0])
 			.end(function(err,res){
-				response = res.body;		
+				response = res.body;					
 				meetingId = response._id;
 				expect(response.serviceId).to.equal(serviceId);
 				expect(res.status).to.equal(200);	
 				done();
 			});
 	});
-	it('3.2 READ a specific meeting',function(done){
+	
+	/**
+	Test case 2: Get the information of a specific meeting
+				 from a specific room
+	*/
+	it('READ a specific meeting',function(done){
 		meetings
 			.getSpecificRoomMeeting(serviceId,roomId[0],meetingId)
 			.end(function(err,res){
@@ -62,7 +81,11 @@ describe('..:: Meetings ::..',function(){
 				done();
 			});
 	});
-	it('3.3 Create a new meeting in a specific room',function(done){
+	
+	/**
+	Test case 3: Create a new meeting in a specific room
+	*/
+	it('CREATE a new meeting in a specific room',function(done){
 		meetings
 			.createNewMeeting(serviceId,roomId[0])
 			.end(function(err,res){
@@ -75,7 +98,10 @@ describe('..:: Meetings ::..',function(){
 			});
 	});
 	
-	it('3.4 Edit a specific meeting in a specific room',function(done){		
+	/**
+	Test case 4: Update a specific meeting in a specific room
+	*/
+	it('UPDATE a specific meeting in a specific room',function(done){		
 		meetings
 			.updateMeeting(serviceId,roomId[0],meetingId)
 			.end(function(err,res){
@@ -88,7 +114,10 @@ describe('..:: Meetings ::..',function(){
 			});
 	});
 	
-	it('3.5 Delete a specific meeting in a specific room',function(done){
+	/**
+	Test case 5: Delete a specific meeting in a specific room
+	*/
+	it('DELETE a specific meeting in a specific room',function(done){
 		
 		meetings
 			.deleteMeeting(serviceId,roomId[0],meetingId)
